34fa1418a1713731b4c4f92e12b78129e5f67b1f
calvin <calvin.francis96@outlook.com>
2021-07-23 09:32:37 -0400

    Update queries

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,53 +8,39 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto 
    FROM characters 
-   ORDER BY LENGTH(motto) DESC 
-   LIMIT 1
+   WHERE length(motto) = (SELECT max(length(motto)) FROM characters);
   "
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) 
-   FROM characters 
-   GROUP BY species 
-   ORDER BY COUNT(species) DESC 
-   LIMIT 1
+  "SELECT characters.species, count(characters.species) FROM characters
+   GROUP BY characters.species
+   ORDER BY characters.species DESC
+   LIMIT 1;
   "
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name 
-   FROM authors 
-   JOIN series 
-   ON series.author_id = authors.id 
-   JOIN subgenres 
-   ON series.subgenre_id = subgenres.id
+  "SELECT authors.name, subgenres.name FROM authors
+   JOIN series ON authors.id = series.author_id
+   JOIN subgenres ON series.subgenre_id = subgenres.id;
   "
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title
-   FROM series
-   JOIN books
-   ON books.series_id = series.id
-   JOIN character_books
-   ON character_books.book_id = books.id
-   JOIN characters
-   ON character_books.character_id = characters.id
-   WHERE characters.species = 'human'
-   GROUP BY series.title
-   ORDER BY COUNT(*) DESC
-   LIMIT 1
+  "SELECT series.title FROM series
+   JOIN characters ON series.author_id = characters.author_id
+   GROUP BY characters.species
+   ORDER BY count(characters.species) DESC
+   LIMIT 1;
   "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(*) as book_count 
-   FROM character_books 
-   JOIN characters 
-   ON character_books.character_id = characters.id 
-   GROUP BY characters.name 
-   ORDER BY book_count DESC, characters.name
+  "SELECT characters.name, count(character_books.book_id) AS book_count FROM characters
+   JOIN character_books ON characters.id = character_books.character_id
+   GROUP BY characters.name
+   ORDER BY book_count DESC, characters.name;
   "
 end
